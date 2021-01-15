819b71cc13128721f45b944398ced85559f087b4
Matthew Scully <p21mscully@fairfieldprep.org>
2021-01-15 13:22:36 -0500

    Project 1/15/21

---
+++ hangman_project.py
@@ -0,0 +1,7 @@
+# Author MrS & TW 1/15/21
+
+word_list = import random.choice(["area", "book", "business", "case", "child", "comany", "country", "day", "eye", "fact", "family", "government", "grou", "hand", "home", "job", "life", "lot", "man", "money", "month", "mother", "night", "number", "part", "people", "place", "point", "problem", "program", "question", "right", "room", "school", "state", "story", "student", "study", "system", "thing", "time", "water", "way", "week", "woman", "word", "work", "world", "year"])
+
+
+print(word_list)
+    
