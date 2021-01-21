b26c22542b77afc2e2eb8e583207125cc600515f
Matthew Scully <p21mscully@fairfieldprep.org>
2021-01-21 09:47:08 -0500

    Word/ game Functions

--- hangman_project.py
+++ hangman_project.py
@@ -1,7 +1,14 @@
 # Author MrS & TW 1/15/21
 
-word_list = import random.choice(["area", "book", "business", "case", "child", "comany", "country", "day", "eye", "fact", "family", "government", "grou", "hand", "home", "job", "life", "lot", "man", "money", "month", "mother", "night", "number", "part", "people", "place", "point", "problem", "program", "question", "right", "room", "school", "state", "story", "student", "study", "system", "thing", "time", "water", "way", "week", "woman", "word", "work", "world", "year"])
+wordlist = import random.choice("area", "book", "business", "case", "child", "comany", "country", "day", "eye", "fact", "family", "government", "grou", "hand", "home", "job", "life", "lot", "man", "money", "month", "mother", "night", "number", "part", "people", "place", "point", "problem", "program", "question", "right", "room", "school", "state", "story", "student", "study", "system", "thing", "time", "water", "way", "week", "woman", "word", "work", "world", "year")
 
+def rand_word():
+    word = random.choice(wordlist)
+    return word.upper()
 
-print(word_list)
-    
+def game(word):
+    word_length = "_" * len(word)
+    guess = False
+    guessed_letters = []
+    guessed_words = []
+    tries =6
